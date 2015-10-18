31475b95be947fd77d813a9038d41fc83cc35099
Christof Stocker <christof.stocker@aimc.at>
2015-10-18 17:22:11 +0200

    add vector uv history

--- src/ValueHistories.jl
+++ src/ValueHistories.jl
@@ -8,12 +8,14 @@ export
 
     ValueHistory,
       UnivalueHistory,
+        VectorUnivalueHistory,
         QueueUnivalueHistory,
       MultivalueHistory,
         DynMultivalueHistory
 
 include("abstract_history.jl")
 include("queue_uv_history.jl")
+include("vector_uv_history.jl")
 include("dyn_mv_history.jl")
 
 end # module
---
+++ src/vector_uv_history.jl
@@ -0,0 +1,31 @@
+type VectorUnivalueHistory{I<:Real,V} <: UnivalueHistory{I}
+  lastiter::I
+  iterations::Vector{I}
+  values::Vector{V}
+
+  function VectorUnivalueHistory(::Type{V}, ::Type{I})
+    new(zero(I), Array(I, 0), Array(V, 0))
+  end
+end
+
+function VectorUnivalueHistory{I,V}(v::Type{V}, i::Type{I} = Int64)
+  VectorUnivalueHistory{I,V}(v, i)
+end
+
+length(history::VectorUnivalueHistory) = length(history.iterations)
+enumerate(history::VectorUnivalueHistory) = zip(history.iterations, history.values)
+first(history::VectorUnivalueHistory) = history.iterations[1], history.values[1]
+last(history::VectorUnivalueHistory) = history.iterations[end], history.values[end]
+get(history::VectorUnivalueHistory) = history.iterations, history.values
+
+function push!{I<:Real,V}(
+    history::VectorUnivalueHistory{I,V},
+    iteration::I,
+    value::V)
+  lastiter = history.lastiter
+  iteration > lastiter || throw(ArgumentError("Iterations must increase over time"))
+  history.lastiter = iteration
+  push!(history.iterations, iteration)
+  push!(history.values, value)
+  value
+end
